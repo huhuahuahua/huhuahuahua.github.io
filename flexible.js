a5a2a50bb0f7e3cbaa4f299d1b445fa2d00e1ac4
huhuahuahua <117599001+huhuahuahua@users.noreply.github.com>
2022-12-01 01:11:26 +0800

    Update flexible.js

--- flexible.js
+++ flexible.js
@@ -5,7 +5,7 @@
     // adjust body font size
     function setBodyFontSize() {
         if (document.body) {
-            document.body.style.fontSize = (11 * dpr) + 'px'
+            document.body.style.fontSize = (80 * dpr) + 'px'
         }
         else {
             document.addEventListener('DOMContentLoaded', setBodyFontSize)
