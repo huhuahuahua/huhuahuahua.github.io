ff46f18ca4411abd493dddd08e4d0b5704748742
huhuahuahua <117599001+huhuahuahua@users.noreply.github.com>
2022-12-01 01:01:48 +0800

    Update flexible.js

--- flexible.js
+++ flexible.js
@@ -5,7 +5,7 @@
     // adjust body font size
     function setBodyFontSize() {
         if (document.body) {
-            document.body.style.fontSize = (10 * dpr) + 'px'
+            document.body.style.fontSize = (11 * dpr) + 'px'
         }
         else {
             document.addEventListener('DOMContentLoaded', setBodyFontSize)
@@ -15,7 +15,7 @@
 
     // set 1rem = viewWidth / 10
     function setRemUnit() {
-        var rem = docEl.clientWidth / 10
+        var rem = docEl.clientWidth / 18
         docEl.style.fontSize = rem + 'px'
     }
 
