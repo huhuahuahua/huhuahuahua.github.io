8da5bf8336e6a32828388ab66a97599d31e99440
huhuahuahua <117599001+huhuahuahua@users.noreply.github.com>
2023-07-27 23:04:37 +0800

    Update verification.js

--- js/verification.js
+++ js/verification.js
@@ -1,15 +1,15 @@
-$('#login-button').click(function (event) {
-    let userName = document.getElementById("userName").value;
-    let pwd = document.getElementById("pwd").value;
-    if (userName == "老6" && pwd == "520") {
-        $('#h').text("生日快乐！");
-        event.preventDefault();
-        $('form').fadeOut(500);
-        $('.wrapper').addClass('form-success');
-        setTimeout(function () {
-            location.href = "BirthdayCake.html";
-        }, 4000);
-    } else {
-        alert("用户名或密码不正确！");
+ $ ( '#login-button' ) . 点击 ( 功能  ( 事件 )  {
+     出租  用户名 = document. 麦田比德 ( "userName" ) . 价值 ;
+     出租  无价值的 = document. 麦田比德 ( "pwd" ) . 价值 ;
+     如果  ( userName == "大傻逼" && pwd == "520" )  {
+         $ ( '#h' ) . 文本 ( "生日快乐！" ) ;
+        事件。 预防违约 ( ) ;
+         $ ( 'form' ) . 逐渐消失 ( 500 ) ;
+         $ ( '.wrapper' ) . 地址类 ( 'form-success' ) ;
+         规定超时时间 ( 功能  ( )  {
+            地点。 高参考值 = "BirthdayCake.html" ;
+         } , 4000 ) ;
+     }  其他的  {
+         警告 ( "用户名或密码不正确！" ) ;
     }
-});
+ } ) ;
